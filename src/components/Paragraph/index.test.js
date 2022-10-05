cc073a54ec111f1366bd84568adbee2e3ed9a5eb
César Galindo <33326122+chestergalindo@users.noreply.github.com>
2022-10-05 15:12:01 -0500

    :pencil2: fix types

--- src/components/Paragraph/index.test.js
+++ src/components/Paragraph/index.test.js
@@ -1,6 +1,6 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import { Paragraph } from './index';
-import { render } from '@testing-library/react';
 
 test('should', () => {
   //const { debug } = render( <Paragraph /> );
--- src/components/Paragraph/index.tsx
+++ src/components/Paragraph/index.tsx
@@ -1,4 +1,8 @@
-// import React from 'react';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore: Unreachable code error
+import React from 'react';
 import styles from './style.module.css';
 
-export const Paragraph = () => <p className={styles.paragraph}> Paragraph</p>;
+export const Paragraph = () => {
+  return <p className={styles.paragraph}> Paragraph</p>;
+};
